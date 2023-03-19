c6acfc7f09f9aa26260b12eec666a3daf1d2aba4
Tofutych <martirosov.davidka@mail.ru>
2023-03-20 01:19:53 +0300

    dish detail & dish ingredients endpoints

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -4,4 +4,6 @@ from product import views
 
 urlpatterns = [
     path('latest-dishes/', views.LatestDishesList.as_view()),
+    path('dish/<int:id>/', views.DishDetail.as_view()),
+    path('dish/<int:id>/ingredients/', views.DishIngredients.as_view())
 ]
