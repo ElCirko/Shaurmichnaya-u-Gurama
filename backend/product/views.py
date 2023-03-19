d57a992032531d2d2e3a7345a3cc996bea086304
Tofutych <martirosov.davidka@mail.ru>
2023-03-19 22:11:42 +0300

    LatestDishesList view

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Dish
+from .serializers import DishSerializer
+
+
+class LatestDishesList(APIView):
+    def get(self, request, format=None):
+        dishes = Dish.objects.all()[0:4]
+        serializer = DishSerializer(dishes, many=True)
+        return Response(serializer.data)
