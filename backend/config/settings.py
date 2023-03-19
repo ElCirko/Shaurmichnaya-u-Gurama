94b2be610d153ac0b6481fd3a22e8171bc0d34e8
Tofutych <martirosov.davidka@mail.ru>
2023-03-19 22:41:02 +0300

    djoser conf

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,13 +23,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'djoser',
 
     'product',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "hhtp://localhost:8080",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
