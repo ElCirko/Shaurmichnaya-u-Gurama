c36ad5bb915f5a9f7b5126e05fae847fafe0f8d4
Tofutych <martirosov.davidka@mail.ru>
2023-03-20 00:15:46 +0300

    navbar + footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,59 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="wrapper">
+    <nav class="navbar is-transparent">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          <strong>U Gurama</strong>
+          <img src="@/assets/logo.svg" alt="shawarma">
+        </router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu"
+          @click="showMobileMenu = !showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{ 'is-active': showMobileMenu }">
+        <div class="navbar-start">
+
+          <router-link to="/shawarma" class="navbar-item">Shawarma</router-link>
+          <router-link to="/drinks" class="navbar-item">Drinks</router-link>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons is-centered">
+              <router-link to="/cart" class="button is-primary">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart ({{ cartTotalLength }})</span>
+              </router-link>
+              <router-link to="/log-in" class="button is-light">Log in</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <router-view />
+  <footer class="footer has-background-white-bis">
+    <div class="content has-text-centered">
+      <p>Shawaright (c) 2023</p>
+    </div>
+  </footer>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
     }
-  }
+  },
 }
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma'
 </style>
