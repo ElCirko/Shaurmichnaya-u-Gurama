e41964feace2455fa6755a96a8ad52292d894c61
Tofutych <martirosov.davidka@mail.ru>
2023-03-20 01:47:01 +0300

    swagger

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,11 +21,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'rest_framework',
-    'rest_framework.authtoken',
+    
     'corsheaders',
     'djoser',
+    'drf_yasg',
+    'rest_framework',
+    'rest_framework.authtoken',
 
     'product',
 ]
