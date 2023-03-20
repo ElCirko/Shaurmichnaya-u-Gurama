7e36b3764b1cf5db4d367cf946bd903f9060779d
Tofutych <martirosov.davidka@mail.ru>
2023-03-20 03:06:18 +0300

    dish by slug

--- backend/product/views.py
+++ backend/product/views.py
@@ -21,7 +21,7 @@ class DishDetail(APIView):
             return Dish.objects.get(id=id)
         except Dish.DoesNotExist:
             raise Http404
-    
+
     def get(self, request, id, format=None):
         dish = self.get_object(id)
         serializer = DishSerializer(dish)
@@ -31,7 +31,27 @@ class DishDetail(APIView):
         dish = self.get_object(id)
         dish.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+
+
+class DishDetailBySlug(APIView):
+    def get_object(self, slug):
+        try:
+            return Dish.objects.get(slug=slug)
+        except Dish.DoesNotExist:
+            raise Http404
+
+    def get(self, request, slug, format=None):
+        dish = self.get_object(slug)
+        serializer = DishSerializer(dish)
+        return Response(serializer.data)
+
+
+class DishList(APIView):
+    def get(self, request, format=None):
+        dishes = Dish.objects.all()
+        serializer = DishSerializer(dishes, many=True)
+        return Response(serializer.data)
+
 
 class DishIngredients(APIView):
     def get_object(self, id):
