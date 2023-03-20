6a048c680458d74df3d36410dccc67972fdc1e37
Tofutych <martirosov.davidka@mail.ru>
2023-03-20 03:06:07 +0300

    some new endpoints

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -5,5 +5,7 @@ from product import views
 urlpatterns = [
     path('latest-dishes/', views.LatestDishesList.as_view()),
     path('dish/<int:id>/', views.DishDetail.as_view()),
+    path('dishes/', views.DishList.as_view()),
+    path('dish/<slug:slug>/', views.DishDetailBySlug.as_view()),
     path('dish/<int:id>/ingredients/', views.DishIngredients.as_view())
 ]
