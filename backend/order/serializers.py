44f7e59d4c0ef41b48f89f32a0579d40f517aad9
Tofutych <martirosov.davidka@mail.ru>
2023-03-25 04:58:49 +0300

    post&put works!

--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from .models import Order, OrderItem
-
+from product.models import Dish
 
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,3 +20,14 @@ class OrderSerializer(serializers.ModelSerializer):
             "order_items",
             "total",
         )
+        
+    def update(self, instance, validated_data):
+        instance.user = validated_data.get("user", instance.user)
+        
+        items = []
+        for item in validated_data.get("order_items"):
+            items.append(OrderItem.objects.get(dish__name=item.get("dish"), quantity=item.get("quantity")))
+        instance.order_items.clear()
+        instance.order_items.add(*items)
+        instance.save()
+        return instance
--- backend/order/views.py
+++ backend/order/views.py
@@ -31,3 +31,11 @@ class OrderDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, id, format=None):
+        order = self.get_object(id)
+        serializer = OrderSerializer(order, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
